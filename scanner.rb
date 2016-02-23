659c3da8760ffbaf67d52274ebd8f8cfa1b1b6b4
Sean Marcia <sean@cs.uoregon.edu>
2016-02-23 13:14:51 -0500

    Updates for new creators website.

--- scanner.rb
+++ scanner.rb
@@ -3,7 +3,7 @@ require "open-uri"
 
 @most_recent_date = File.open('recent_date.txt', 'rb') { |file| file.read }
 
-doc = Nokogiri::HTML(open("http://www.creators.com/opinion/authors.html"))
+doc = Nokogiri::HTML(open("http://www.creators.com/read/walter-williams"))
 
 def format_date(date)
   date.strftime("%Y-%m-%d-")
@@ -20,18 +20,20 @@ def markdown_title
   "#{@date}#{@title}.md"
 end
 
-@page_date = doc.at('.name:contains("Walter")').text.strip.gsub(/Walter E. WilliamsUpdated/, "")
+@page_date = doc.css(".top").text
+#.at('.name:contains("Walter")').text.strip.gsub(/Walter E. WilliamsUpdated/, "")
 
 if @most_recent_date != @page_date
   @date = format_date(Date.parse(@page_date))
-  article = Nokogiri::HTML(open("http://www.creators.com/opinion/walter-williams.html"))
-  @article_title = article.css("h1").text.gsub(":","&#58;")
+  article = Nokogiri::HTML(open("http://www.creators.com/read/walter-williams"))
+  @article_title = article.css(".article_title")
   @title = format_title(article.css("h1").text)
-  @body = article.css(".KonaBody").text
-  @body = @body.gsub(/\n\n\n\n\nloadAdVals\(\"AD FEEDBACK\"\);\n/, "")
+  @body = article.css("#article-content").text
+  @body = @body.gsub("  ", "")
+  @body = @body.gsub(/\n\n\n\n\n/, "\n")
   @body = @body.gsub(/\n/, "\n\n")
 
-  open("/home/sean/wewio/_posts/"+markdown_title, 'w') do |f|
+  open("/Users/sean/ruby/walterewilliams.github.io/_posts/"+markdown_title, 'w') do |f|
     f.puts "---"
     f.puts "layout: post"
     f.puts "title:  #{@article_title}"
@@ -45,4 +47,3 @@ if @most_recent_date != @page_date
     f.print @page_date
   end
 end
-
